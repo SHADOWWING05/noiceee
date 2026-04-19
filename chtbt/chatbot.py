5f1ecdf6eadbec60e8aeec7bbddaec91dac97f75
SHADOWWING05 <omegafluke5@gmail.com>
2026-04-19 20:08:38 +0530

    Update chatbot.py

--- chtbt/chatbot.py
+++ chtbt/chatbot.py
@@ -20,7 +20,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 app = Flask(__name__)
 
 # Sets an environment variable GOOGLE_API_KEY with a specified API key.
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAOyEQ99eLYTABWN9mrrryPqw3BjgNAsPc"; 
+os.environ["GOOGLE_API_KEY"] = "c"; 
 
 # Defines a route for the home page (/) that sends the index.html file from the web directory.
 @app.route('/')
