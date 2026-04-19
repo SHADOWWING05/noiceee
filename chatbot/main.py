f4dccf6d74d3af41c77cffbf5b6b23c75083df1e
SHADOWWING05 <omegafluke5@gmail.com>
2026-04-19 20:08:13 +0530

    Update main.py

--- chatbot/main.py
+++ chatbot/main.py
@@ -4,7 +4,7 @@ import streamlit as st
 import os
 import google.generativeai as genai
 
-GOOGLE_API_KEY = "AIzaSyClTbdWPM7ZQ5RIBAwwagO0Cmkty7xpTUI"
+GOOGLE_API_KEY = ""
 genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel("gemini-pro")
